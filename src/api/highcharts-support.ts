32034be128a2bd0ac62a89240652d849e062073d
Mikhail Fursov <mike.fursov@gmail.com>
2016-01-30 22:05:28 -0800

    Minor code cleanup

--- src/api/highcharts-support.ts
+++ src/api/highcharts-support.ts
@@ -3,23 +3,24 @@ import {getAccountInfo, AccountInfoResponse} from "./investflow-rest";
 
 const HIGHCHARTS_MODAL_DIV_ID = "iflow_highcharts_modal";
 
-const HIGHCHARTS_MODAL_DIV_HTML = `<div class="modal" tabindex="-1" role="dialog">
-            <div class="modal-dialog">
+const HIGHCHARTS_MODAL_DIV_HTML =
+    `<div class="modal" tabindex="-1" role="dialog">
+        <div class="modal-dialog">
             <div class="modal-content">
-            <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            <h4 class="modal-title">Доходность счета</h4>
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title">Доходность счета</h4>
+                </div>
+                <div class="modal-body">
+                    <div class="iflow-modal-chart"></div>
+                </div>
+                <div class="modal-footer">
+                    <a class="btn btn-success account-url" target="_blank" >Страница счета</a>
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Закрыть</button>
+                 </div>
             </div>
-            <div class="modal-body">
-                <div class="iflow-modal-chart"></div>
-            </div>
-            <div class="modal-footer">
-            <a class="btn btn-success account-url" target="_blank" >Страница счета</a>
-            <button type="button" class="btn btn-default" data-dismiss="modal">Закрыть</button>
-            </div>
-            </div><!-- /.modal-content -->
-            </div><!-- /.modal-dialog -->
-            </div><!-- /.modal -->`;
+        </div>
+    </div>`;
 
 function showChart(accountInfo:AccountInfoResponse) {
     // find modal window, create if not found
@@ -58,13 +59,12 @@ function showChart(accountInfo:AccountInfoResponse) {
         },
         tooltip: {
             xDateFormat: "%Y-%m-%d",
-            valueSuffix: "%",
-            shared: true
+            valueSuffix: "%"
         },
         plotOptions: {
             line: {
                 dataLabels: {
-                    format: "{y}%",
+                    format: "{point.y:.2f}%",
                     enabled: true
                 }
             }
