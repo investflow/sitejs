4e97d37cd2f9f35f1a69d22f4660812aa1996c09
Mikhail Fursov <mike.fursov@gmail.com>
2016-02-01 22:24:32 -0800

    Support for graph tabs for accounts

--- src/api/highcharts-support.ts
+++ src/api/highcharts-support.ts
@@ -22,40 +22,51 @@ const HIGHCHARTS_MODAL_DIV_HTML =
         </div>
     </div>`;
 
-function showChart(accountInfo:AccountInfoResponse) {
-    // find modal window, create if not found
-    let $modalDiv = $("#" + HIGHCHARTS_MODAL_DIV_ID);
-    if ($modalDiv.length === 0) {
-        $modalDiv = $("<div/>", {id: HIGHCHARTS_MODAL_DIV_ID}).appendTo(document.body);
+function getRangeButtons(firstEventMillis:number, lastEventMillis:number):Array<any> {
+    let buttons:Array<any> = [];
+    let millisPerMonth = 30 * 24 * 60 * 60 * 1000;
+    let rangeMillis = lastEventMillis - firstEventMillis;
+
+    if (rangeMillis >= millisPerMonth) {
+        buttons.push({type: "month", count: 1, text: "1м"});
     }
+    if (rangeMillis >= 3 * millisPerMonth) {
+        buttons.push({type: "month", count: 3, text: "3м"});
+    }
+    if (rangeMillis >= 6 * millisPerMonth) {
+        buttons.push({type: "month", count: 6, text: "6м"});
+    }
+    if (rangeMillis >= millisPerMonth) {
+        buttons.push({type: "ytd", text: "YTD"});
+    }
+    if (rangeMillis >= 12 * millisPerMonth) {
+        buttons.push({type: "year", count: 1, text: "Год"});
+    }
+    buttons.push({type: "all", count: 1, text: "Все"});
+    return buttons;
+}
 
-    // reset old window contents
-    $modalDiv.empty();
-    $modalDiv.append(HIGHCHARTS_MODAL_DIV_HTML);
-    let fullAccountName = accountInfo.name + "/" + accountInfo.account;
-    $modalDiv.find(".modal-title").text("Доходность счета " + fullAccountName);
-    $modalDiv.find(".account-url").attr("href", accountInfo.url);
 
-    var $dialog = $modalDiv.find(".modal-dialog");
-    $dialog.width(window.innerWidth * 0.8);
-    $dialog.height(window.innerHeight * 0.8);
-
-    // set new contents
-    var profitHistory = accountInfo.profitHistory;
-    let options = {
+function prepareProfitChartOptions(fullAccountName:string, profitHistory:Array<Array<number>>):any {
+    let firstEventMillis = -1;
+    let lastEventMillis = -1;
+    if (profitHistory.length > 0) {
+        firstEventMillis = profitHistory[0][0];
+        lastEventMillis = profitHistory[profitHistory.length - 1][0];
+    }
+    let buttons:Array<any> = getRangeButtons(firstEventMillis, lastEventMillis);
+    return {
         title: {
             text: "",
         },
         credits: {
             enabled: false
         },
-        chart: {
-            width: $dialog.width() - 30,
-            height: $dialog.height() - 30
-        },
+        chart: {},
         rangeSelector: {
             allButtonsEnabled: true,
-            selected: 5 // All button
+            buttons: buttons,
+            selected: buttons.length - 1 // All button
         },
         tooltip: {
             xDateFormat: "%Y-%m-%d",
@@ -77,7 +88,31 @@ function showChart(accountInfo:AccountInfoResponse) {
             }
         }]
     };
+}
 
+function showChart(accountInfo:AccountInfoResponse) {
+    // find modal window, create if not found
+    let $modalDiv = $("#" + HIGHCHARTS_MODAL_DIV_ID);
+    if ($modalDiv.length === 0) {
+        $modalDiv = $("<div/>", {id: HIGHCHARTS_MODAL_DIV_ID}).appendTo(document.body);
+    }
+
+    // reset old window contents
+    $modalDiv.empty();
+    $modalDiv.append(HIGHCHARTS_MODAL_DIV_HTML);
+    let fullAccountName = accountInfo.name + "/" + accountInfo.account;
+    $modalDiv.find(".modal-title").text("Доходность счета " + fullAccountName);
+    $modalDiv.find(".account-url").attr("href", accountInfo.url);
+
+    var $dialog = $modalDiv.find(".modal-dialog");
+    $dialog.width(window.innerWidth * 0.8);
+    $dialog.height(window.innerHeight * 0.8);
+
+    // set new contents
+    var profitHistory = accountInfo.profitHistory;
+    var options = prepareProfitChartOptions(fullAccountName, profitHistory);
+    options.chart.width = $dialog.width() - 30;
+    options.chart.height = $dialog.height() - 30;
     $modalDiv.find(".iflow-modal-chart").highcharts("StockChart", options);
 
     // show modal window
@@ -92,5 +127,10 @@ export default {
                 }
             );
         });
+    },
+
+    addAccountChart: function (elementSelector:string, fullAccountName:string, profitHistory:Array<Array<number>>) {
+        var options = prepareProfitChartOptions(fullAccountName, profitHistory);
+        $(elementSelector).highcharts("StockChart", options);
     }
 }
