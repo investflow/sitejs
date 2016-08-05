6ccca6fc2ec17a9f28e9009cddb76baf91e73045
Mikhail Fursov <fursovm@amazon.com>
2016-08-04 20:23:43 -0700

    correct shim & tds for highlight.js

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -9,5 +9,5 @@
 /// <reference path="parsley/parsley.d.ts" />
 /// <reference path="flot/jquery.flot.d.ts" />
 /// <reference path="autolinker/autolinker.d.ts" />
-/// <reference path="highlight-js/highlight-js.d.ts" />
+/// <reference path="highlightjs/highlightjs.d.ts" />
 
